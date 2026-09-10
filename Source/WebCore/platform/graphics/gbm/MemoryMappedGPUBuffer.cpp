3303c6f5ddfd9c91fc415d6b0dcc7e4166c7c016
Fujii Hironori <fujii@igalia.com>
2026-09-10 02:19:05 -0700

    [GTK][WPE] white noise images for dma_buf atlas on i915 + Intel Arc https://bugs.webkit.org/show_bug.cgi?id=311103
    Reviewed by Nikolas Zimmermann.
    
    The combination of i915 driver and Intel Arc caused white noise for atlas image
    uploading. Using xe driver work fine for the video card.
    
    Using CPUMappingStrategy::GBMBoMap and unmapping in ~AccessScope() can work
    around the issue for i915.
    
    * Source/WebCore/platform/graphics/gbm/MemoryMappedGPUBuffer.cpp:
    (WebCore::runCapabilityProbe):
    (WebCore::MemoryMappedGPUBuffer::AccessScope::~AccessScope):
    
    Canonical link: https://commits.webkit.org/320811@main

--- Source/WebCore/platform/graphics/gbm/MemoryMappedGPUBuffer.cpp
+++ Source/WebCore/platform/graphics/gbm/MemoryMappedGPUBuffer.cpp
@@ -46,6 +46,7 @@
 
 #if USE(LIBDRM)
 #include <drm_fourcc.h>
+#include <xf86drm.h>
 #endif
 
 namespace WebCore {
@@ -122,6 +123,13 @@ static CPUMappingStrategy runCapabilityProbe()
         return CPUMappingStrategy::Unsupported;
     }
 
+    // CPUMappingStrategy::DmaBufFDMmap doesn't work for the combination of i915 driver and Intel Arc.
+    bool preferGBMBoMap = false;
+    if (drmVersion* version = drmGetVersion(gbm_device_get_fd(gbmDevice->device()))) {
+        preferGBMBoMap = equalSpans(unsafeSpan(version->name), "i915"_span);
+        drmFreeVersion(version);
+    }
+
     // mmap capability depends on the kernel dma-buf subsystem and the GBM backend, not on
     // buffer size, format, or modifier -- so the single-plane linear probe generalizes.
     static constexpr int probeSize = 16;
@@ -134,11 +142,13 @@ static CPUMappingStrategy runCapabilityProbe()
 
     auto strategy = CPUMappingStrategy::Unsupported;
 
-    if (auto* bo = createProbeBO()) {
-        UnixFileDescriptor fd { gbm_bo_get_fd_for_plane(bo, 0), UnixFileDescriptor::Adopt };
-        if (fd && probeReadWriteMappability(fd.value(), gbm_bo_get_stride_for_plane(bo, 0) * probeSize))
-            strategy = CPUMappingStrategy::DmaBufFDMmap;
-        gbm_bo_destroy(bo);
+    if (!preferGBMBoMap) {
+        if (auto* bo = createProbeBO()) {
+            UnixFileDescriptor fd { gbm_bo_get_fd_for_plane(bo, 0), UnixFileDescriptor::Adopt };
+            if (fd && probeReadWriteMappability(fd.value(), gbm_bo_get_stride_for_plane(bo, 0) * probeSize))
+                strategy = CPUMappingStrategy::DmaBufFDMmap;
+            gbm_bo_destroy(bo);
+        }
     }
 
     if (strategy == CPUMappingStrategy::Unsupported) {
@@ -484,6 +494,9 @@ MemoryMappedGPUBuffer::AccessScope::AccessScope(MemoryMappedGPUBuffer& buffer, A
 MemoryMappedGPUBuffer::AccessScope::~AccessScope()
 {
     m_buffer.performDMABufSyncSystemCall({ DMABufSyncFlag::End, m_mode == AccessScope::Mode::Read ? DMABufSyncFlag::Read : DMABufSyncFlag::Write });
+
+    if (m_mode == Mode::Write && cachedCPUMappingStrategy() == CPUMappingStrategy::GBMBoMap)
+        m_buffer.unmapIfNeeded();
 }
 
 std::unique_ptr<MemoryMappedGPUBuffer::AccessScope> MemoryMappedGPUBuffer::AccessScope::create(MemoryMappedGPUBuffer& buffer, AccessScope::Mode mode)
