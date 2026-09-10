81f7a618cee3d34485aa4fb70fa8efdd37431528
Tyler Wilcock <tyler_w@apple.com>
2026-09-10 11:12:52 -0700

    AX: In isolated tree mode, backwards word and sentence navigation fail to leave a paragraph https://bugs.webkit.org/show_bug.cgi?id=323701 rdar://186957255
    Reviewed by Dominic Mazzoni.
    
    AXTextMarker::findWordOrSentence walks objects with text runs in the search direction,
    flattening their text so the word and sentence break iterators have enough context, and
    stopped as soon as the containing block changed. That means a marker at the start of a
    paragraph never saw the paragraph before it, so AXPreviousWordStartTextMarkerForTextMarker
    and AXPreviousSentenceStartTextMarkerForTextMarker returned the marker they were given, and
    VoiceOver couldn't move backwards by word or sentence past the start of a block.
    
    With this commit, we let the backwards walk cross a containing-block change, joining the two
    blocks' text with the newline they render as, so the break iterators don't run the last word
    of one paragraph into the first word of the next.
    
    Makes text-marker-p-tags.html pass in ITM.
    
    * LayoutTests/accessibility-isolated-tree/TestExpectations:
    * LayoutTests/accessibility/isolated-tree/mac/text-marker-p-tags-expected.txt: Added.
    * LayoutTests/accessibility/isolated-tree/mac/text-marker-p-tags.html: Added.
    * Source/WebCore/accessibility/AXTextMarker.cpp:
    (WebCore::AXTextMarker::findWordOrSentence const):
    
    Canonical link: https://commits.webkit.org/320840@main

--- Source/WebCore/accessibility/AXTextMarker.cpp
+++ Source/WebCore/accessibility/AXTextMarker.cpp
@@ -1686,12 +1686,15 @@ AXTextMarker AXTextMarker::findWordOrSentence(AXDirection direction, bool findWo
 
     // objectBorder maintains the position in flattenedRuns between the current object's text and the previously scanned object(s)
     int objectBorder = direction == AXDirection::Next ? 0 : flattenedRuns.length();
+    // Position in flattenedRuns of the boundary the backwards iterators last found, if they found one.
+    std::optional<int> backwardsBoundaryIndex;
 
     // Functions to update resultMarker for word and sentence text units.
     auto updateWordResultMarker = [&] () {
         if (direction == AXDirection::Previous && boundary == AXTextUnitBoundary::Start) {
             TEXT_MARKER_ASSERT_SINGLE(offset <= flattenedRuns.length(), (*this));
             int previousWordStart = findNextWordFromIndex(flattenedRuns, offset, false);
+            backwardsBoundaryIndex = previousWordStart;
             if (previousWordStart <= objectBorder)
                 resultMarker = AXTextMarker(*currentObject, previousWordStart, origin);
         } else if (direction == AXDirection::Next && boundary == AXTextUnitBoundary::End) {
@@ -1718,12 +1721,16 @@ AXTextMarker AXTextMarker::findWordOrSentence(AXDirection direction, bool findWo
     auto updateSentenceResultMarker = [&] () {
         if (boundary == AXTextUnitBoundary::Start) {
             int start = previousSentenceStartFromOffset(flattenedRuns, offset);
+            if (direction == AXDirection::Previous && start != -1)
+                backwardsBoundaryIndex = start;
             if (direction == AXDirection::Previous && start < objectBorder && start != -1)
                 resultMarker = AXTextMarker(*currentObject, start, origin);
             else if (direction == AXDirection::Next && start != -1 && start >= objectBorder)
                 resultMarker = AXTextMarker(*currentObject, start - objectBorder, origin);
         } else {
             int end = nextSentenceEndFromOffset(flattenedRuns, offset);
+            if (direction == AXDirection::Previous && end != -1)
+                backwardsBoundaryIndex = end;
             // If the current marker (this) is the same position from the end, start a new search from there.
             if (direction == AXDirection::Previous && end <= objectBorder && end != -1)
                 resultMarker = AXTextMarker(*currentObject, end, origin);
@@ -1732,6 +1739,12 @@ AXTextMarker AXTextMarker::findWordOrSentence(AXDirection direction, bool findWo
                 resultMarker = AXTextMarker(*currentObject, end - objectBorder, origin);
             }
         }
+    };
+
+    // The boundary can't move once scanned text precedes it, because text further back only adds
+    // words and sentences before it.
+    auto foundFinalBoundary = [&] {
+        return backwardsBoundaryIndex.value_or(0) > 0;
     };
 
     while (currentObject) {
@@ -1743,9 +1756,16 @@ AXTextMarker AXTextMarker::findWordOrSentence(AXDirection direction, bool findWo
         bool lastObjectIsEditable = !!currentObject->editableAncestor();
         currentObject = findObjectWithRuns(*currentObject, direction);
         if (currentObject) {
-            // We should return when the containing block is different (indicating a paragraph).
-            if (currentRuns->containingBlock != currentObject->textRuns()->containingBlock)
-                return resultMarker;
+            bool crossedContainingBlock = currentRuns->containingBlock != currentObject->textRuns()->containingBlock;
+            if (crossedContainingBlock) {
+                // A containing block change means a new paragraph. Only backwards searches cross one. e.g.:
+                //   <p>Test sentence one. Test sentence two</p>
+                //   <p>|Where</p>
+                // The previous word start is "two". Forwards from "two|" the next word end is the
+                // paragraph break itself, so nothing in the next paragraph needs searching.
+                if (direction == AXDirection::Next || foundFinalBoundary())
+                    return resultMarker;
+            }
 
             // We only stop at line breaks when finding words, as for sentences, the text break iterator needs to find the next sentence boundary, which isn't necessarily at a break.
             bool shouldStopAtLineBreaks = findWord && currentObject->role() == AccessibilityRole::LineBreak && !currentObject->editableAncestor();
@@ -1757,8 +1777,10 @@ AXTextMarker AXTextMarker::findWordOrSentence(AXDirection direction, bool findWo
             currentRuns = currentObject->textRuns();
             StringView newRunsFlattenedString = currentRuns->toStringView();
             if (direction == AXDirection::Previous) {
-                flattenedRuns = makeString(newRunsFlattenedString, flattenedRuns);
-                offset += newRunsFlattenedString.length();
+                // Crossing a containing block means a new paragraph. Include a \n separator in this case.
+                unsigned separatorLength = crossedContainingBlock ? 1 : 0;
+                flattenedRuns = crossedContainingBlock ? makeString(newRunsFlattenedString, '\n', flattenedRuns) : makeString(newRunsFlattenedString, flattenedRuns);
+                offset += newRunsFlattenedString.length() + separatorLength;
                 objectBorder = newRunsFlattenedString.length();
             } else {
                 // We don't need to update the offset when moving fowards, since text is being appended to the end of flattenedRuns
