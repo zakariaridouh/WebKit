zakariaridouh/WebKit

commit 972966afe6de9b139ab0d53809b9eb5dbcedb69b

Only allow sending to _WKRemoteObjectRegistry of a page in the sending process rdar://176912453
Reviewed by Chris Dumez.

If a compromised web content process somehow gets the identifier of another WebPageProxy
in the same WebProcessPool, it could send messages to that page's _WKRemoteObjectRegistry.
This reconfigures the registration and message dispatching so that only a page that is
known to be used by the current WebProcessProxy can receive IPC to its _WKRemoteObjectRegistry.

* Source/WebKit/Shared/API/Cocoa/RemoteObjectRegistry.h:
* Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:
(-[WKWebView dealloc]):
(-[WKWebView _remoteObjectRegistry]):
* Source/WebKit/UIProcess/Cocoa/WebPageProxyCocoa.mm:
(WebKit::WebPageProxy::remoteObjectRegistry):
(WebKit::WebPageProxy::uiRemoteObjectRegistry):
* Source/WebKit/UIProcess/PageClient.h:
* Source/WebKit/UIProcess/WebPageProxy.h:
* Source/WebKit/UIProcess/WebProcessProxy.cpp:
(WebKit::WebProcessProxy::handleRemoteObjectRegistryMessage):
(WebKit::WebProcessProxy::dispatchMessage):
* Source/WebKit/UIProcess/WebProcessProxy.h:
* Source/WebKit/UIProcess/mac/PageClientImplMac.h:
* Source/WebKit/UIProcess/mac/PageClientImplMac.mm:
(WebKit::PageClientImpl::remoteObjectRegistry): Deleted.
* Source/WebKit/UIProcess/mac/WebPageProxyMac.mm:
(WebKit::WebPageProxy::remoteObjectRegistry): Deleted.
* Source/WebKit/UIProcess/mac/WebViewImpl.mm:
(WebKit::WebViewImpl::~WebViewImpl):
(WebKit::WebViewImpl::remoteObjectRegistry):

Originally-landed-as: 305413.962@safari-7624-branch (dcf51f6e5ef7). rdar://180437303
Canonical link: https://commits.webkit.org/316400@main

Source/WebKit/UIProcess/Cocoa/WebPageProxyCocoa.mm
@@ -54,10 +54,11 @@
 #import "SafeBrowsingUtilities.h"
 #import "SharedBufferReference.h"
 #import "SynapseSPI.h"
+#import "UIRemoteObjectRegistry.h"
 #import "VideoPresentationManagerProxy.h"
 #import "WKErrorInternal.h"
 #import "WKHistoryDelegatePrivate.h"
-#import "WKWebView.h"
+#import "WKWebViewPrivate.h"
 #import "WebContextMenuProxy.h"
 #import "WebEventModifier.h"
 #import "WebFrameProxy.h"
@@ -72,6 +73,7 @@
 #import "WebProcessProxy.h"
 #import "WebScreenOrientationManagerProxy.h"
 #import "WebsiteDataStore.h"
+#import "_WKRemoteObjectRegistryInternal.h"
 #import <Foundation/NSURLRequest.h>
 #import <WebCore/AXObjectCache.h>
 #import <WebCore/AppHighlight.h>
@@ -663,6 +665,18 @@ ResourceError WebPageProxy::errorForUnpermittedAppBoundDomainNavigation(const UR
 }
 
 WebPageProxy::Internals::~Internals() = default;
+
+_WKRemoteObjectRegistry *WebPageProxy::remoteObjectRegistry()
+{
+    return [cocoaView() _remoteObjectRegistry];
+}
+
+RemoteObjectRegistry* WebPageProxy::uiRemoteObjectRegistry()
+{
+    if (RetainPtr registry = remoteObjectRegistry())
+        return &[registry remoteObjectRegistry];
+    return nullptr;
+}
 
 #if ENABLE(APPLE_PAY)
 

Source/WebKit/UIProcess/WebProcessProxy.cpp
@@ -127,6 +127,8 @@
 #include <wtf/text/WTFString.h>
 
 #if PLATFORM(COCOA)
+#include "RemoteObjectRegistry.h"
+#include "RemoteObjectRegistryMessages.h"
 #include <wtf/cocoa/RuntimeApplicationChecksCocoa.h>
 #endif
 
@@ -1343,6 +1345,43 @@ bool WebProcessProxy::shouldAllowNonValidInjectedCode() const
 }
 #endif
 
+#if PLATFORM(COCOA)
+bool WebProcessProxy::handleRemoteObjectRegistryMessage(IPC::Connection& connection, IPC::Decoder& decoder)
+{
+    if (!WebPageProxyIdentifier::isValidIdentifier(decoder.destinationID()))
+        return false;
+    WebPageProxyIdentifier pageID(decoder.destinationID());
+
+    auto receiveMessage = [&] (WebPageProxy& page) {
+        if (RefPtr registry = page.uiRemoteObjectRegistry()) {
+            registry->didReceiveMessage(connection, decoder);
+            return true;
+        }
+        return false;
+    };
+
+    if (RefPtr page = m_pageMap.get(pageID))
+        return receiveMessage(*page);
+
+    for (Ref remotePage : m_remotePages) {
+        if (RefPtr page = remotePage->page(); page && page->identifier() == pageID)
+            return receiveMessage(*page);
+    }
+
+    for (Ref provisionalPage : m_provisionalPages) {
+        if (RefPtr page = provisionalPage->page(); page && page->identifier() == pageID)
+            return receiveMessage(*page);
+    }
+
+    for (Ref suspendedPage : m_suspendedPages) {
+        if (RefPtr page = suspendedPage->page(); page && page->identifier() == pageID)
+            return receiveMessage(*page);
+    }
+
+    return false;
+}
+#endif
+
 bool WebProcessProxy::dispatchMessage(IPC::Connection& connection, IPC::Decoder& decoder)
 {
     // If AuxiliaryProcessProxy gets .messages.in, use WantsDispatchMessages and remove this.
@@ -1350,7 +1389,12 @@ bool WebProcessProxy::dispatchMessage(IPC::Connection& connection, IPC::Decoder&
         return true;
     if (protect(processPool())->dispatchMessage(connection, decoder))
         return true;
-    if (decoder.messageReceiverName() == Messages::WebFrameProxy::messageReceiverName()) {
+    auto messageName = decoder.messageReceiverName();
+#if PLATFORM(COCOA)
+    if (messageName == Messages::RemoteObjectRegistry::messageReceiverName())
+        return handleRemoteObjectRegistryMessage(connection, decoder);
+#endif
+    if (messageName == Messages::WebFrameProxy::messageReceiverName()) {
         if (RefPtr frame = FrameIdentifier::isValidIdentifier(decoder.destinationID()) ? WebFrameProxy::webFrame(FrameIdentifier(decoder.destinationID())) : nullptr)
             frame->didReceiveMessage(connection, decoder);
         else

Source/WebKit/Shared/API/Cocoa/RemoteObjectRegistry.h
@@ -51,6 +51,9 @@ public:
     void sendReplyBlock(uint64_t replyID, const UserData& blockInvocation);
     void sendUnusedReply(uint64_t replyID);
 
+    // IPC::MessageReceiver
+    void didReceiveMessage(IPC::Connection&, IPC::Decoder&) override;
+
 protected:
     explicit RemoteObjectRegistry(_WKRemoteObjectRegistry *);
     using MessageSender = Variant<std::reference_wrapper<WebProcessProxy>, std::reference_wrapper<WebPage>>;
@@ -59,9 +62,6 @@ private:
     virtual std::optional<MessageSender> messageSender() = 0;
     virtual std::optional<uint64_t> messageDestinationID() = 0;
     template<typename M> void send(M&&);
-
-    // IPC::MessageReceiver
-    void didReceiveMessage(IPC::Connection&, IPC::Decoder&) override;
 
     // Message handlers
     void invokeMethod(const RemoteObjectInvocation&);

Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm
@@ -969,9 +969,6 @@ static void addBrowsingContextControllerMethodStubsIfNeeded()
 
 #if PLATFORM(IOS_FAMILY)
     [_contentView _webViewDestroyed];
-
-    if (_page && _remoteObjectRegistry)
-        protect(_page->configuration().processPool())->removeMessageReceiver(Messages::RemoteObjectRegistry::messageReceiverName(), _page->identifier());
 #endif
 
     if (_page)
@@ -4712,10 +4709,8 @@ static RetainPtr<NSArray> wkTextManipulationErrors(NSArray<_WKTextManipulationIt
 #if PLATFORM(MAC)
     return _impl->remoteObjectRegistry();
 #else
-    if (!_remoteObjectRegistry) {
+    if (!_remoteObjectRegistry)
         _remoteObjectRegistry = adoptNS([[_WKRemoteObjectRegistry alloc] _initWithWebPageProxy:*_page]);
-        protect(_page->configuration().processPool())->addMessageReceiver(Messages::RemoteObjectRegistry::messageReceiverName(), _page->identifier(), protect([_remoteObjectRegistry remoteObjectRegistry]));
-    }
 
     return _remoteObjectRegistry.get();
 #endif

Source/WebKit/UIProcess/PageClient.h
@@ -555,7 +555,6 @@ public:
     virtual void setShouldSuppressFirstResponderChanges(bool) = 0;
 
     virtual RetainPtr<NSView> inspectorAttachmentView() = 0;
-    virtual _WKRemoteObjectRegistry *remoteObjectRegistry() = 0;
 
     virtual void intrinsicContentSizeDidChange(const WebCore::IntSize& intrinsicContentSize) = 0;
 

Source/WebKit/UIProcess/WebPageProxy.h
@@ -541,6 +541,7 @@ class RemoteLayerTreeScrollingPerformanceData;
 class RemoteLayerTreeTransaction;
 class RemoteMediaSessionCoordinatorProxy;
 class RemoteMediaSessionManagerProxy;
+class RemoteObjectRegistry;
 class RemotePageProxy;
 class RemoteScrollingCoordinatorProxy;
 class RevealItem;
@@ -1350,6 +1351,9 @@ public:
     void scrollToEdge(WebCore::RectEdges<bool>, WebCore::ScrollIsAnimated);
 
 #if PLATFORM(COCOA)
+    _WKRemoteObjectRegistry* remoteObjectRegistry();
+    RemoteObjectRegistry* uiRemoteObjectRegistry();
+
     void windowAndViewFramesChanged(const WebCore::FloatRect& viewFrameInWindowCoordinates, const WebCore::FloatPoint& accessibilityViewCoordinates);
     void setMainFrameIsScrollable(bool);
     bool shouldDelayWindowOrderingForEvent(const WebMouseEvent&);
@@ -1409,7 +1413,6 @@ public:
     void rootViewToWindow(const WebCore::IntRect& viewRect, WebCore::IntRect& windowRect);
 
     RetainPtr<NSView> inspectorAttachmentView();
-    _WKRemoteObjectRegistry *remoteObjectRegistry();
 
     CGRect boundsOfLayerInLayerBackedWindowCoordinates(CALayer *) const;
 #endif // PLATFORM(MAC)

Source/WebKit/UIProcess/WebProcessProxy.h
@@ -660,8 +660,10 @@ private:
     void connectionWillOpen(IPC::Connection&) override;
     void processWillShutDown(IPC::Connection&) override;
     bool shouldSendPendingMessage(const IPC::Encoder&) final;
-    
+
 #if PLATFORM(COCOA)
+    bool handleRemoteObjectRegistryMessage(IPC::Connection&, IPC::Decoder&);
+
     void cacheMediaMIMETypesInternal(const Vector<String>&);
 #endif
 

Source/WebKit/UIProcess/mac/PageClientImplMac.h
@@ -284,7 +284,6 @@ private:
 #endif
 
     RetainPtr<NSView> inspectorAttachmentView() override;
-    _WKRemoteObjectRegistry *remoteObjectRegistry() override;
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
     WebCore::WebMediaSessionManager& mediaSessionManager() final;

commit 48b9b5b5fcd3aea3a9d9eaca4648354db3b6301d

Ignore messages to RemoteObjectRegistry if destination can't be found https://bugs.webkit.org/show_bug.cgi?id=323811 rdar://184175210
Reviewed by Brady Eidson.

This fixes a regression from 316400@main.

Before 316400@main, if a message was sent to a RemoteObjectRegistry that
had been destroyed in the UI process, WebProcessProxy::dispatchMessage
would fall through to the final return true; and we would ignore the message
and move on.  Since that change, we return handleRemoteObjectRegistryMessage
which, to match behavior, needs to return true if we can't find the
RemoteObjectRegistry.  Since the RemoteObjectRegistry's lifetime is tied to
the WKWebView and not the WebPageProxy, it is not uncommon for it to be destroyed
before the WebPageProxy during teardown, hence the radar.

Test: ipc/remote-object-registry-invoke-method-stale-page.html

* LayoutTests/ipc/remote-object-registry-invoke-method-stale-page-expected.txt: Added.
* LayoutTests/ipc/remote-object-registry-invoke-method-stale-page.html: Added.
* Source/WebKit/Shared/API/Cocoa/_WKRemoteObjectRegistry.mm:
(-[_WKRemoteObjectRegistry remoteObjectRegistry]):
* Source/WebKit/Shared/API/Cocoa/_WKRemoteObjectRegistryInternal.h:
* Source/WebKit/UIProcess/Cocoa/WebPageProxyCocoa.mm:
(WebKit::WebPageProxy::uiRemoteObjectRegistry):
* Source/WebKit/UIProcess/WebProcessProxy.cpp:
(WebKit::WebProcessProxy::handleRemoteObjectRegistryMessage):

Canonical link: https://commits.webkit.org/320837@main

Source/WebKit/Shared/API/Cocoa/_WKRemoteObjectRegistry.mm
@@ -187,9 +187,9 @@ static uint64_t NODELETE generateReplyIdentifier()
     remoteObjectRegistry->sendInvocation(WebKit::RemoteObjectInvocation(interface.identifier, [encoder rootObjectDictionary], WTF::move(replyInfo)));
 }
 
-- (WebKit::RemoteObjectRegistry&)remoteObjectRegistry
+- (WebKit::RemoteObjectRegistry*)remoteObjectRegistry
 {
-    return *_remoteObjectRegistry;
+    return _remoteObjectRegistry.get();
 }
 
 static NSString *replyBlockSignature(Protocol *protocol, SEL selector, NSUInteger blockIndex)

Source/WebKit/Shared/API/Cocoa/_WKRemoteObjectRegistryInternal.h
@@ -39,7 +39,7 @@ class WebPageProxy;
 
 @interface _WKRemoteObjectRegistry ()
 
-@property (nonatomic, readonly) WebKit::RemoteObjectRegistry& remoteObjectRegistry;
+@property (nonatomic, readonly) WebKit::RemoteObjectRegistry* remoteObjectRegistry;
 
 - (id)_initWithWebPage:(std::reference_wrapper<WebKit::WebPage>)messageSender;
 - (id)_initWithWebPageProxy:(std::reference_wrapper<WebKit::WebPageProxy>)messageSender;

Source/WebKit/UIProcess/Cocoa/WebPageProxyCocoa.mm
@@ -659,7 +659,7 @@ _WKRemoteObjectRegistry *WebPageProxy::remoteObjectRegistry()
 RemoteObjectRegistry* WebPageProxy::uiRemoteObjectRegistry()
 {
     if (RetainPtr registry = remoteObjectRegistry())
-        return &[registry remoteObjectRegistry];
+        return [registry remoteObjectRegistry];
     return nullptr;
 }
 

Source/WebKit/UIProcess/WebProcessProxy.cpp
@@ -1607,16 +1607,17 @@ bool WebProcessProxy::handleRemoteObjectRegistryMessage(IPC::Connection& connect
         return false;
 
     WebPageProxyIdentifier pageID(decoder.destinationID());
-    if (!isAssociatedWithPage(pageID))
-        return false;
 
     RefPtr page = WebPageProxy::fromIdentifier(pageID);
     if (!page)
+        return true;
+
+    if (!isAssociatedWithPage(pageID))
         return false;
 
     RefPtr registry = page->uiRemoteObjectRegistry();
     if (!registry)
-        return false;
+        return true;
 
     registry->didReceiveMessage(connection, decoder);
     return true;

LayoutTests/ipc/remote-object-registry-invoke-method-stale-page-expected.txt
@@ -1,0 +1,3 @@
+Tests that a RemoteObjectRegistry message addressed to a page that no longer exists is dropped instead of terminating the WebContent process.
+
+PASS if no crash.

LayoutTests/ipc/remote-object-registry-invoke-method-stale-page.html
@@ -1,0 +1,39 @@
+<!DOCTYPE html><!-- webkit-test-runner [ IPCTestingAPIEnabled=true IgnoreInvalidMessageWhenIPCTestingAPIEnabled=false ] -->
+<html>
+<head>
+<script>
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+}
+
+function done()
+{
+    document.getElementById("result").textContent = "PASS if no crash.";
+    if (window.testRunner)
+        testRunner.notifyDone();
+}
+
+async function main()
+{
+    if (!window.IPC)
+        return done();
+
+    const stalePageProxyID = IPC.webPageProxyID + 1000000n;
+
+    for (const messageName of ["RemoteObjectRegistry_InvokeMethod", "RemoteObjectRegistry_CallReplyBlock", "RemoteObjectRegistry_ReleaseUnusedReplyBlock"]) {
+        IPC.sendMessage("UI", stalePageProxyID, IPC.messages[messageName].name, new Uint8Array(0));
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+    done();
+}
+
+window.onload = () => { main().catch(done); };
+</script>
+</head>
+<body>
+<p>Tests that a RemoteObjectRegistry message addressed to a page that no longer exists is dropped instead of terminating the WebContent process.</p>
+<p id="result">FAIL</p>
+</body>
+</html>