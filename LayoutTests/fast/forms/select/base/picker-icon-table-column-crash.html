zakariaridouh/WebKit

commit d18de66a088832f557f7b29e0df08f416530f448

Enhanced <select>: add ::picker-icon pseudo-element https://bugs.webkit.org/show_bug.cgi?id=306734 rdar://169398829
Reviewed by Said Abou-Hallawa.

A pseudo element will be created for the `::picker-icon`. The default behavior
for the renderer of this pseudo element is to create a `content` renderer which
displays a down-arrow character.

Specs: https://drafts.csswg.org/css-forms-1/#picker-icon

* LayoutTests/TestExpectations:
* LayoutTests/imported/w3c/web-platform-tests/css/css-pseudo/parsing/the-picker-icon-pseudo-element.tentative-expected.txt:
* Source/JavaScriptCore/inspector/protocol/CSS.json:
* Source/WebCore/animation/WebAnimationUtilities.cpp:
(WebCore::compareStyleOriginatedAnimationOwningElementPositionsInDocumentTreeOrder):
(WebCore::pseudoElementIdentifierAsString):
* Source/WebCore/css/CSSPseudoSelectors.json:
* Source/WebCore/css/CSSSelector.cpp:
(WebCore::CSSSelector::stylePseudoElementTypeFor):
* Source/WebCore/css/html.css:
(select::picker-icon):
* Source/WebCore/css/parser/CSSParserContext.cpp:
(WebCore::applyUASheetBehaviorsToContext):
(WebCore::add):
* Source/WebCore/cssjit/SelectorCompiler.cpp:
(WebCore::SelectorCompiler::constructFragmentsInternal):
* Source/WebCore/inspector/agents/InspectorCSSAgent.cpp:
(WebCore::InspectorCSSAgent::protocolValueForPseudoElementType):
* Source/WebCore/rendering/style/RenderStyleConstants.cpp:
(WebCore::operator<<):
* Source/WebCore/rendering/style/RenderStyleConstants.h:
* Source/WebCore/rendering/updating/RenderTreeBuilder.cpp:
(WebCore::RenderTreeBuilder::updateAfterDescendants):
* Source/WebCore/rendering/updating/RenderTreeBuilderFormControls.cpp:
(WebCore::RenderTreeBuilder::FormControls::updateAfterDescendants):
(WebCore::RenderTreeBuilder::FormControls::updatePseudoElement):
(WebCore::RenderTreeBuilder::FormControls::updateCheckmark): Deleted.
* Source/WebCore/rendering/updating/RenderTreeBuilderFormControls.h:
* Source/WebCore/style/StyleTreeResolver.cpp:

Source/WebCore/rendering/updating/RenderTreeBuilderFormControls.cpp
@@ -27,11 +27,13 @@
 #include "RenderTreeBuilderFormControls.h"
 
 #include "HTMLInputElement.h"
+#include "HTMLSelectElement.h"
 #include "InputType.h"
 #include "RenderBlockFlow.h"
 #include "RenderBlockInlines.h"
 #include "RenderButton.h"
 #include "RenderDescendantIterator.h"
+#include "RenderMenuList.h"
 #include "RenderTreeBuilderBlock.h"
 #include "RenderTreeUpdaterGeneratedContent.h"
 #include "Settings.h"
@@ -86,59 +88,62 @@ void RenderTreeBuilder::FormControls::updateAfterDescendants(RenderElement& rend
             return;
 
         if (inputType->isCheckable())
-            updateCheckmark(renderer);
+            updatePseudoElement(PseudoElementType::Checkmark, renderer);
+        return;
+    }
+
+    if (RefPtr select = dynamicDowncast<HTMLSelectElement>(renderer.element()); select && select->usesMenuList()) {
+        updatePseudoElement(PseudoElementType::PickerIcon, renderer);
+        return;
     }
 }
 
-void RenderTreeBuilder::FormControls::updateCheckmark(RenderElement& renderer)
+void RenderTreeBuilder::FormControls::updatePseudoElement(PseudoElementType type, RenderElement& renderer)
 {
-    RefPtr inputElement = dynamicDowncast<HTMLInputElement>(renderer.element());
-    ASSERT(inputElement);
-
-    auto pseudoStyle = renderer.getCachedPseudoStyle({ PseudoElementType::Checkmark });
+    auto pseudoStyle = renderer.getCachedPseudoStyle({ type });
     if (!pseudoStyle)
         return;
 
-    auto shouldHaveCheckmarkRenderer = [&]() -> bool {
-        return renderer.style().appearance() == StyleAppearance::Base && pseudoStyle->display() != DisplayType::None;
+    auto shouldHavePseudoElementRenderer = [&]() -> bool {
+        return renderer.style().usedAppearance() == StyleAppearance::Base && pseudoStyle->display() != DisplayType::None;
     };
 
-    auto existingCheckmark = [&]() -> CheckedPtr<RenderBlockFlow> {
+    auto existingPseudoElement = [&]() -> CheckedPtr<RenderElement> {
         for (CheckedRef child : childrenOfType<RenderElement>(renderer)) {
-            if (child->style().pseudoElementType() == PseudoElementType::Checkmark)
-                return dynamicDowncast<RenderBlockFlow>(child);
+            if (child->style().pseudoElementType() == type)
+                return child;
         }
         return nullptr;
     }();
 
-    if (!shouldHaveCheckmarkRenderer()) {
-        if (existingCheckmark)
-            m_builder.destroy(*existingCheckmark);
+    if (!shouldHavePseudoElementRenderer()) {
+        if (existingPseudoElement)
+            m_builder.destroy(*existingPseudoElement);
         return;
     }
 
-    if (existingCheckmark && existingCheckmark->style().content() == pseudoStyle->content()) {
-        auto checkmarkStyle = RenderStyle::clone(*pseudoStyle);
-        existingCheckmark->setStyle(WTF::move(checkmarkStyle));
-        RenderTreeUpdater::GeneratedContent::updateStyleForContentRenderers(*existingCheckmark, existingCheckmark->style());
+    if (existingPseudoElement && existingPseudoElement->style().content() == pseudoStyle->content()) {
+        auto pseudoElementStyle = RenderStyle::clone(*pseudoStyle);
+        existingPseudoElement->setStyle(WTF::move(pseudoElementStyle));
+        RenderTreeUpdater::GeneratedContent::updateStyleForContentRenderers(*existingPseudoElement, existingPseudoElement->style());
         return;
     }
 
-    if (existingCheckmark) {
-        m_builder.destroy(*existingCheckmark);
-        existingCheckmark = nullptr;
+    if (existingPseudoElement) {
+        m_builder.destroy(*existingPseudoElement);
+        existingPseudoElement = nullptr;
     }
 
     Ref document = renderer.document();
-    auto checkmarkStyle = RenderStyle::clone(*pseudoStyle);
+    auto pseudoElementStyle = RenderStyle::clone(*pseudoStyle);
 
-    RenderPtr<RenderBlockFlow> checkmark = createRenderer<RenderBlockFlow>(RenderObject::Type::BlockFlow, document, WTF::move(checkmarkStyle));
-    checkmark->initializeStyle();
+    RenderPtr<RenderBlockFlow> pseudoElement = createRenderer<RenderBlockFlow>(RenderObject::Type::BlockFlow, document, WTF::move(pseudoElementStyle));
+    pseudoElement->initializeStyle();
 
-    if (checkmark->style().hasContent())
-        RenderTreeUpdater::GeneratedContent::createContentRenderers(m_builder, *checkmark, checkmark->style(), PseudoElementType::Checkmark);
+    if (pseudoElement->style().hasContent())
+        RenderTreeUpdater::GeneratedContent::createContentRenderers(m_builder, *pseudoElement, pseudoElement->style(), type);
 
-    m_builder.attach(renderer, WTF::move(checkmark));
+    m_builder.attach(renderer, WTF::move(pseudoElement));
 }
 
 } // namespace WebCore

LayoutTests/TestExpectations
@@ -683,6 +683,15 @@ imported/w3c/web-platform-tests/html/semantics/forms/the-select-element/customiz
 imported/w3c/web-platform-tests/html/semantics/forms/the-select-element/select-many-options.tentative.html [ Skip ]
 imported/w3c/web-platform-tests/html/semantics/forms/the-select-element/select-multiple-size-1.tentative.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/html/semantics/forms/the-select-element/select-popover-position-with-zoom.tentative.html [ Skip ]
+imported/w3c/web-platform-tests/html/semantics/forms/the-select-element/select-marker-end-aligned.tentative.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-height-in-flex.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-size-empty-001.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-size-empty-002.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-width-in-flex.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-1-block-size.html [ ImageOnlyFailure Pass ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-button-min-height-001.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-empty.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/css/cssom/getComputedStyle-pseudo-picker-icon.html [ Pass Failure ]
 
 # Flaky crashing test.
 media/modern-media-controls/tracks-support/no-tracks.html [ Crash Pass Failure ]
@@ -909,8 +918,6 @@ imported/w3c/web-platform-tests/html/rendering/non-replaced-elements/the-page/bo
 imported/w3c/web-platform-tests/html/rendering/non-replaced-elements/the-page/body-margin-2l.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/html/rendering/replaced-elements/embedded-content/change-src-while-not-displayed.html [ ImageOnlyFailure Pass ]
 imported/w3c/web-platform-tests/html/rendering/replaced-elements/images/input-image-content.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-empty.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-button-min-height-001.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/html/rendering/widgets/button-layout/anonymous-button-content-box.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/html/rendering/widgets/button-layout/display-none-or-contents.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/html/rendering/widgets/button-layout/input-type-button-clip.html [ ImageOnlyFailure ]
@@ -8155,11 +8162,6 @@ imported/w3c/web-platform-tests/css/css-break/inline-skipping-fragmentainer-001.
 imported/w3c/web-platform-tests/css/css-break/table/repeated-section/text-in-nested-multicol.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/css-break/text-indent-and-wide-float.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/css-break/transform-025.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-size-empty-001.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-size-empty-002.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/select-1-block-size.html [ ImageOnlyFailure Pass ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-height-in-flex.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/html/rendering/replaced-elements/the-select-element/customizable-select/min-width-in-flex.html [ ImageOnlyFailure ]
 
 imported/w3c/web-platform-tests/html/rendering/sections/headingoffset-and-headingreset.html [ ImageOnlyFailure ]
 

LayoutTests/imported/w3c/web-platform-tests/css/css-pseudo/parsing/the-picker-icon-pseudo-element.tentative-expected.txt
@@ -1,7 +1,7 @@
 
-FAIL "::picker-icon" should be a valid selector '::picker-icon' is not a valid selector.
-FAIL "*::picker-icon" should be a valid selector '*::picker-icon' is not a valid selector.
-FAIL "foo.bar[baz]::picker-icon" should be a valid selector 'foo.bar[baz]::picker-icon' is not a valid selector.
+PASS "::picker-icon" should be a valid selector
+PASS "*::picker-icon" should be a valid selector
+PASS "foo.bar[baz]::picker-icon" should be a valid selector
 PASS "::picker-icon *" should be an invalid selector
 PASS "::picker-icon::picker-icon" should be an invalid selector
 PASS "::before::picker-icon" should be an invalid selector
@@ -14,5 +14,5 @@ PASS "::picker-icon::marker" should be an invalid selector
 PASS "::picker-icon::placeholder" should be an invalid selector
 PASS "::slotted(*)::picker-icon::slotted(*)" should be an invalid selector
 FAIL "::slotted(*)::picker-icon" should be a valid selector '::slotted(*)::picker-icon' is not a valid selector.
-FAIL "::part(foo)::picker-icon" should be a valid selector '::part(foo)::picker-icon' is not a valid selector.
+PASS "::part(foo)::picker-icon" should be a valid selector
 

Source/WebCore/animation/WebAnimationUtilities.cpp
@@ -65,7 +65,7 @@ static bool compareStyleOriginatedAnimationOwningElementPositionsInDocumentTreeO
     //     - any other pseudo-elements not mentioned specifically in this list, sorted in ascending order by the Unicode codepoints that make up each selector
     //     - ::after
     //     - element children
-    enum SortingIndex : uint8_t { NotPseudo, Marker, Before, FirstLetter, FirstLine, GrammarError, Highlight, WebKitScrollbar, Selection, SpellingError, TargetText, Checkmark, After, ViewTransition, ViewTransitionGroup, ViewTransitionImagePair, ViewTransitionOld, ViewTransitionNew, Other };
+    enum SortingIndex : uint8_t { NotPseudo, Marker, Before, FirstLetter, FirstLine, GrammarError, Highlight, WebKitScrollbar, Selection, SpellingError, TargetText, Checkmark, After, PickerIcon, ViewTransition, ViewTransitionGroup, ViewTransitionImagePair, ViewTransitionOld, ViewTransitionNew, Other };
     auto sortingIndex = [](const std::optional<Style::PseudoElementIdentifier>& pseudoElementIdentifier) -> SortingIndex {
         if (!pseudoElementIdentifier)
             return NotPseudo;
@@ -95,6 +95,8 @@ static bool compareStyleOriginatedAnimationOwningElementPositionsInDocumentTreeO
             return After;
         case PseudoElementType::Checkmark:
             return Checkmark;
+        case PseudoElementType::PickerIcon:
+            return PickerIcon;
         case PseudoElementType::ViewTransition:
             return ViewTransition;
         case PseudoElementType::ViewTransitionGroup:
@@ -331,6 +333,7 @@ String pseudoElementIdentifierAsString(const std::optional<Style::PseudoElementI
     static NeverDestroyed<const String> spellingError(MAKE_STATIC_STRING_IMPL("::spelling-error"));
     static NeverDestroyed<const String> targetText(MAKE_STATIC_STRING_IMPL("::target-text"));
     static NeverDestroyed<const String> checkmark(MAKE_STATIC_STRING_IMPL("::checkmark"));
+    static NeverDestroyed<const String> pickerIcon(MAKE_STATIC_STRING_IMPL("::picker-icon"));
     static NeverDestroyed<const String> viewTransition(MAKE_STATIC_STRING_IMPL("::view-transition"));
     static NeverDestroyed<const String> webkitScrollbar(MAKE_STATIC_STRING_IMPL("::-webkit-scrollbar"));
     switch (pseudoElementIdentifier->type) {
@@ -356,6 +359,8 @@ String pseudoElementIdentifierAsString(const std::optional<Style::PseudoElementI
         return targetText;
     case PseudoElementType::Checkmark:
         return checkmark;
+    case PseudoElementType::PickerIcon:
+        return pickerIcon;
     case PseudoElementType::ViewTransition:
         return viewTransition;
     case PseudoElementType::ViewTransitionGroup:

Source/WebCore/css/CSSSelector.cpp
@@ -304,6 +304,8 @@ std::optional<PseudoElementType> CSSSelector::stylePseudoElementTypeFor(PseudoEl
         return PseudoElementType::After;
     case PseudoElement::Checkmark:
         return PseudoElementType::Checkmark;
+    case PseudoElement::PickerIcon:
+        return PseudoElementType::PickerIcon;
     case PseudoElement::WebKitScrollbar:
         return PseudoElementType::WebKitScrollbar;
     case PseudoElement::WebKitScrollbarButton:

Source/WebCore/css/html.css
@@ -1093,6 +1093,13 @@ select:not([multiple]):-internal-uses-menulist > button:first-child {
     /* FIXME: add display: contents; once it works as expected */
 }
 
+select::picker-icon {
+    /* FIXME: Switch to disclosure-open counter once it works */
+    content: "\25BC" / "";
+    display: block;
+    margin-inline-start: auto;
+}
+
 optgroup {
     font-weight: bolder;
 }

Source/WebCore/css/parser/CSSParserContext.cpp
@@ -60,6 +60,7 @@ static void applyUASheetBehaviorsToContext(CSSParserContext& context)
     context.propertySettings.useSystemAppearance = true;
 #endif
     context.cssInternalAutoBaseParsingEnabled = true;
+    context.htmlEnhancedSelectEnabled = true;
 }
 
 CSSParserContext::CSSParserContext(CSSParserMode mode, const URL& baseURL)
@@ -146,6 +147,7 @@ void add(Hasher& hasher, const CSSParserContext& context)
 #endif
         context.colorLayersEnabled,
         context.targetTextPseudoElementEnabled,
+        context.htmlEnhancedSelectEnabled,
         context.cssRandomFunctionEnabled,
         context.cssTreeCountingFunctionsEnabled,
         context.cssURLModifiersEnabled,
@@ -157,7 +159,6 @@ void add(Hasher& hasher, const CSSParserContext& context)
         context.cssTextTransformMathAutoEnabled,
         context.cssInternalAutoBaseParsingEnabled,
         context.cssMathDepthEnabled,
-        context.htmlEnhancedSelectEnabled,
         context.openPseudoClassEnabled
     );
     add(hasher, context.baseURL, context.charset, context.propertySettings, context.mode, bits);

Source/WebCore/cssjit/SelectorCompiler.cpp
@@ -1520,6 +1520,7 @@ static FunctionType constructFragmentsInternal(const CSSSelector& rootSelector, 
             case CSSSelector::PseudoElement::InternalWritingSuggestions:
             case CSSSelector::PseudoElement::Marker:
             case CSSSelector::PseudoElement::Picker:
+            case CSSSelector::PseudoElement::PickerIcon:
             case CSSSelector::PseudoElement::WebKitResizer:
             case CSSSelector::PseudoElement::WebKitScrollbar:
             case CSSSelector::PseudoElement::WebKitScrollbarButton:

commit 2bbc270ed64cf3f873a6e01455c305fa040e6b19

RenderTreeBuilder::FormControls::updatePseudoElement attaches a ::picker-icon renderer to a RenderTableCol https://bugs.webkit.org/show_bug.cgi?id=323585 rdar://185183091
Reviewed by Tim Nguyen.

RenderTreeBuilder::FormControls::updatePseudoElement() attaches the ::picker-icon and
::checkmark renderers to the form control's renderer without consulting
canHaveChildren() or isChildAllowed(), unlike every other renderer creation path. A
<select> with appearance: base-select and display: table-column therefore gets an
anonymous RenderBlockFlow under its RenderTableCol, breaking the invariant that
nextColumn() relies on when it hard-casts firstChild() with downcast<RenderTableCol>().
Check both before attaching, which leaves firstChild() null so nextColumn() falls
through to its already type-filtered sibling walk. Both are needed: table-column is
rejected by canHaveChildren(), table-column-group by isChildAllowed(). The checks sit
after the existing-renderer teardown, and before createContentRenderers() and
setPseudoElementRenderer(), so nothing stale is kept and the discarded renderer is a
leaf with no pointer published to the parent. Not a regression from 307421@main: the
unguarded attach predates it in updateCheckmark() and is still reachable via
::checkmark with an author rule, but select::picker-icon applies to every base
appearance menulist <select>, so plain markup now reaches it.

Test: fast/forms/select/base/picker-icon-table-column-crash.html

* LayoutTests/fast/forms/select/base/picker-icon-table-column-crash-expected.txt: Added.
* LayoutTests/fast/forms/select/base/picker-icon-table-column-crash.html: Added.
* Source/WebCore/rendering/updating/RenderTreeBuilderFormControls.cpp:
(WebCore::RenderTreeBuilder::FormControls::updatePseudoElement):

Canonical link: https://commits.webkit.org/320996@main

Source/WebCore/rendering/updating/RenderTreeBuilderFormControls.cpp
@@ -143,11 +143,17 @@ void RenderTreeBuilder::FormControls::updatePseudoElement(PseudoElementType type
         existingPseudoElement = nullptr;
     }
 
+    if (!renderer.canHaveChildren())
+        return;
+
     Ref document = renderer.document();
     auto pseudoElementStyle = Style::ComputedStyle::clone(*pseudoStyle);
 
     RenderPtr<RenderBlockFlow> pseudoElement = createRenderer<RenderBlockFlow>(RenderObject::Type::BlockFlow, document, WTF::move(pseudoElementStyle));
     pseudoElement->initializeStyle();
+
+    if (!renderer.isChildAllowed(*pseudoElement, pseudoElement->style()))
+        return;
 
     if (pseudoElement->style().content().isData())
         RenderTreeUpdater::GeneratedContent::createContentRenderers(m_builder, *pseudoElement, pseudoElement->style(), type);

LayoutTests/fast/forms/select/base/picker-icon-table-column-crash-expected.txt
@@ -1,0 +1,11 @@
+A base-appearance select whose display turns it into a table column must not get a ::picker-icon renderer attached to its RenderTableCol.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS getComputedStyle(document.getElementById("column")).display is "table-column"
+PASS getComputedStyle(document.getElementById("columnGroup")).display is "table-column-group"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/select/base/picker-icon-table-column-crash.html
@@ -1,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../../../resources/js-test.js"></script>
+<style>
+select, ::picker(select) {
+    appearance: base-select;
+}
+</style>
+</head>
+<body>
+<select id="column" style="display: table-column"><option>One</option></select>
+<select id="columnGroup" style="display: table-column-group"><option>Two</option></select>
+<script>
+description("A base-appearance select whose display turns it into a table column must not get a ::picker-icon renderer attached to its RenderTableCol.");
+
+document.body.offsetHeight;
+
+shouldBeEqualToString('getComputedStyle(document.getElementById("column")).display', 'table-column');
+shouldBeEqualToString('getComputedStyle(document.getElementById("columnGroup")).display', 'table-column-group');
+</script>
+</body>
+</html>